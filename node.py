30ba9ffba639e8d4cea74e9385602ce024e4b29b
Quentin Rossettini <quentin.rossettini@mail.utoronto.ca>
2018-03-06 16:58:46 +0100

    node

---
+++ node.py
@@ -0,0 +1,56 @@
+import random
+import cattle
+
+class Node:
+    def __init__(self, neighbours, I, n, code):
+        self.neighbours = neighbours
+        self.I = I
+        self.n = n
+        self.code = code
+        self.tau = random.randint(I//2, I)
+        self.c = 0
+        self.t = 0
+        self.inconsistent = False
+        self.buffer = set()
+
+    def broadcast(self, broadcast_code):
+        if broadcast_code:
+            code_to_broadcast = self.code
+        else:
+            code_to_broadcast = None
+        for neighbour in self.neighbours:
+            neighbour.receive(self.n, code_to_broadcast)
+
+    def receive(self, n, code):
+        self.buffer.add((n, code))
+
+    def add_neighbour(self, neighbour):
+        self.neighbours.add(neighbour)
+
+    def remove_neighbour(self, neighbour):
+        self.neighbours.remove(neighbour)
+
+    def update(self, n, code):
+        self.n = n
+        self.code = code
+
+    def tick(self):
+        for message in self.buffer:
+            if message[0] == self.n:
+                self.c += 1
+            elif message[0] < self.n:
+                self.broadcast(True)
+                self.inconsistent = True
+            elif message[0] > self.n and message[1] is None:
+                self.broadcast(False)
+                self.inconsistent = True
+            else:
+                self.update(*message)
+                self.inconsistent = True
+        if self.t == self.tau and self.c < self.k:
+            self.broadcast(False)
+        if self.t == self.I:
+            if not self.inconsistent:
+                self.I = min()
+
+
