60cdaa1a77bb76eb34e77df9d7fc0b857eb5ad8d
Ayaz Badouraly <ayaz.badouraly@via.ecp.fr>
2018-03-20 14:16:35 +0100

    Adding random updates

--- main.py
+++ main.py
@@ -1,25 +1,46 @@
 from cattle import Cattle
+from random import randint
+import time
 
-cattle = Cattle()
+def create_cattle():
+    cattle = Cattle()
 
-A = cattle.new_node("A", 2)
-B = cattle.new_node("B", 1)
-C = cattle.new_node("C", 1)
-D = cattle.new_node("D", 1)
-E = cattle.new_node("E", 1)
+    A = cattle.new_node("A", 2, connected=True)
+    B = cattle.new_node("B", 1)
+    C = cattle.new_node("C", 1)
+    D = cattle.new_node("D", 1)
+    E = cattle.new_node("E", 1)
 
 
-A.add_neighbour(B)
-A.add_neighbour(D)
+    A.add_neighbour(B)
+    A.add_neighbour(D)
 
-B.add_neighbour(C)
-B.add_neighbour(E)
+    B.add_neighbour(C)
+    B.add_neighbour(E)
 
-C.add_neighbour(E)
+    C.add_neighbour(E)
 
-D.add_neighbour(E)
+    D.add_neighbour(E)
 
-E.add_neighbour(A)
-E.add_neighbour(B)
+    E.add_neighbour(A)
+    E.add_neighbour(B)
 
-cattle.start(0.2)
+    return cattle
+
+if __name__ == "__main__":
+    cattle = create_cattle()
+    counter = 0
+    next_update = counter + randint(50, 100)
+
+    while True:
+        if counter = next_update:
+            [updated_node] = random.sample(cattle.connected_nodes, 1)
+            updated_node.node += 1
+            print(f"main: update node {updated_node.name} to version {updated_node.n}")
+            next_update = counter + randint(50, 100)
+            print(f"main: next update will occur at t={next_update}")
+
+        cattle.tick()
+        time.sleep(0.1)
+
+
