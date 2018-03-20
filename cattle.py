bc6f8db58061ebc7b2cd1e3f1dd86d9ea6853066
Quentin Rossettini <quentin.rossettini@mail.utoronto.ca>
2018-03-20 14:18:52 +0100

    added connected_nodes and remove_node

--- cattle.py
+++ cattle.py
@@ -15,16 +15,30 @@ class Cattle:
         self.i_min = 10
         self.k = 5
         self.time = 0
+        self.connected_nodes = set()
+
 
     @property
     def i_max(self):
         return self.i_min * (2 ** self.max)
 
-    def new_node(self, name, n):
+    def new_node(self, name, n, connected=False):
         node = Node(name=name, n=n, i=self.i_min, k=self.k, imin=self.i_min, imax=self.i_max)
         self.nodes.add(node)
         print(f"Added node {node.name}")
+        if connected:
+            self.connected_nodes.add(node)
         return node
+    
+    def remove_node(self, node=None, name=None):
+        if not node and not name:
+            raise ValueError("Must specify either a node or a name")
+        if node:
+            self.nodes.discard(node)
+            self.connected_nodes.discard(node)
+        elif name:
+            self.nodes.discard(self.get_node_by_name(name))
+            self.connected_nodes.discard(self.get_node_by_name(name))
 
     def tick(self):
         self.time += 1
